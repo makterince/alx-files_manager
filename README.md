c27f312721baaf25d1e6e2e059c179482d4f3050
Festus Omaku <fomaku3@gmail.com>
2023-10-01 16:23:49 +0100

    redis

---
+++ README.md
@@ -0,0 +1,1 @@
+this is a task to build a file manager with the skills i have acquired during the course of my backend specialization training
---
+++ utils/redis.js
@@ -0,0 +1,30 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.error(`Redis client not connected to the server: ${err.message}`));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    return asyncGet(key);
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
